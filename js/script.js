582ca7a392ce5464a60d05f146e0aff47514ab55
Another <Another@main.com>
2023-01-21 22:48:03 +0300

    JS_Project_Step_1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-let Another = "I'm in the system";
+"use strict";
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastWatchedMovie1 = prompt("Один из последних просмотренных фильмов?", "");
+let rateOfMovie1 = +prompt("Насколько оцените его?", "0");
+let lastWatchedMovie2 = prompt("Один из последних просмотренных фильмов?", "");
+let rateOfMovie2 = +prompt("Насколько оцените его?", "0");
+
+personalMovieDB.movies[lastWatchedMovie1] = rateOfMovie1;
+personalMovieDB.movies[lastWatchedMovie2] = rateOfMovie2;
+
+console.log(personalMovieDB.movies);
